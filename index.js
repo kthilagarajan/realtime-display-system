88b151566ced21a14290c5856d789bd571bd4067
Thilagarajan Kamarajan <kthilagarajan@gmail.com>
2020-05-26 11:24:37 +0530

    MVP

---
+++ index.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app)
+const io = require('socket.io')(server);
+const bodyParser = require('body-parser')
+const path = require('path');
+const _ = require('lodash');
+const moment = require('moment');
+const Geo = require('geo-nearby');
+const port = 2020;
+
+const stations = require('./dataset/stations-formatted.json');
+const stationCompactSet = Geo.createCompactSet(stations, { id: ['code'], name: ['name'] });
+const stationsGeo = new Geo(stationCompactSet, { sorted: true });
+
+const trains = require('./dataset/trains.json');
+const schedules = require('./dataset/schedules.json');
+
+let connectedClients = 0;
+
+app.use(bodyParser.json());
+app.use('/', express.static(path.join(__dirname, "/static")));
+app.get('/ping', (req, res) => {
+    res.send('** Indian Railways API **')
+});
+
+server.listen(port, () => console.log(`listening on port ${port}!`))
+
+function isVaidUser(req, res, next) {
+    // Authentication
+    // req.headers.auth
+    next();
+}
+
+app.post('/stations', isVaidUser, (req, res) => {
+    let [lat, long] = [req.body.lat, req.body.long];
+    let response = {
+        status: 200,
+        data: []
+    }
+    if (lat && long) {
+        let stations = stationsGeo.nearBy(lat, long, 25000) // 5 KM
+        let data = _.map(stations, (f) => {
+            return {
+                code: f.i,
+                name: f.name
+            };
+        })
+        response.data = _.sortBy(data, "name")
+    } else {
+        let data = _.map(stations.features, (f) => {
+            return {
+                code: f.properties.code,
+                name: f.properties.name
+            };
+        });
+        response.data = _.sortBy(data, "name")
+    }
+    res.send(response)
+});
+
+app.get('/trains/:code', isVaidUser, (req, res) => {
+    let code = req.params.code;
+    let response = {
+        status: 200,
+        data: []
+    }
+    if (code) {
+        let result = (trains.from[code] || []).concat(trains.to[code] || [])
+        response.data = _.sortBy(result, "departure");
+    } else {
+        response.status(404);
+    }
+    res.send(response)
+});
+
+app.post('/:code/train/status', isVaidUser, (req, res) => {
+    let code = req.params.code;
+    let response = {
+        status: 200,
+        data: []
+    }
+    let currentTime = moment().utcOffset("+05:30").format("HH:mm:ss")
+    // .format("HH:mm:ss")
+    for (let type of ["from", "to"]) {
+        let scheduledTrains = trains[type][code]
+        for (let t of scheduledTrains) {
+            let message = ``
+            for (let s of schedules[t.number]) {
+                let diff = moment.duration(moment(currentTime, "HH:mm:ss").diff(moment(s.departure, "HH:mm:ss")))
+                    .asMinutes()
+                message = `At ${s.station_name} [${s.station_code}]` 
+                let arrivalDiff = moment.duration(moment(currentTime, "HH:mm:ss").diff(moment(s.arrival, "HH:mm:ss")))
+                    .asMinutes()
+                if(arrivalDiff < 0 && arrivalDiff + 3 < 0) {
+                    message = `Upcoming ${s.station_name} [${s.station_code}]`
+                }
+                if (diff < 0) {
+                    break;
+                }
+            }
+            io.sockets.emit(code, {
+                trainNo: t.number,
+                message
+            })
+        }
+    }
+    res.send(response)
+});
+
+io.on('connection', client => {
+    // console.log(`Connected ${client.id}`)
+    connectedClients++;
+    console.log("Connected Clients : " + connectedClients)
+    client.on('disconnect', () => {
+        // console.log(`Disconnected ${client.id}`)
+        connectedClients--;
+    });
+});
---
+++ static/scripts/railway.js
@@ -0,0 +1,108 @@
+$(document).ready(() => {
+  let lat, long;
+  $(".loader").toggle("show");
+  $(".error-gps").toggle("hide");
+
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition, errorPosition);
+    } else {
+      $(".error-gps").toggle("show");
+      $(".loader").toggle("hide");
+    }
+  }
+
+  function showPosition(position) {
+    lat = position.coords.latitude;
+    long = position.coords.longitude;
+    $(".lat-input").val(lat);
+    $(".long-input").val(long);
+    $(".loader").toggle("hide");
+    getStations();
+  }
+
+  function errorPosition(error) {
+    if (error.code == error.PERMISSION_DENIED) {
+      console.log("Location Permission Denied");
+    }
+    getStations();
+    $(".error-gps").toggle("show");
+    $(".loader").toggle("hide");
+  }
+
+  async function getStations() {
+    let resposne = await fetch("/stations", {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ lat, long })
+    });
+    let result = await resposne.json();
+    var template = $("#select-options").html();
+    var rendered = Mustache.render(template, { options: result.data });
+    if (lat && long) {
+      $(".nearby-station-select").html(rendered)
+    } else {
+      $(".all-station-select").html(rendered)
+    }
+  }
+  getLocation();
+
+
+
+  socket.on('disconnect', function () {
+
+  });
+});
+var socket = io.connect(`${window.location.href}`);
+socket.on('connect', function () {
+  console.log('connect ' + socket.id);
+});
+async function findTrains(selector) {
+  let stationCode = $(selector).val();
+  let resposne = await fetch(`/trains/${stationCode}`, {
+    method: "GET",
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    }
+  });
+  let result = await resposne.json();
+  let trains = result.data;
+  if(trains.length > 0) {
+    socket.on(stationCode, function (data) {
+      $(`.${data.trainNo} .status`).html(data.message)
+      // console.log(data)
+    });
+    var template = $("#train-record-row").html();
+    var rendered = Mustache.render(template, { trains });
+    $(".train-records").html(rendered)
+  } else {
+    alert("No trains found! Try with different station!")
+  }
+}
+let interval = null;
+async function findTrainUpdates(selector) {
+  if (interval) {
+    clearInterval(interval)
+  }
+  getLiveFeeds($(selector).val())
+  interval = setInterval(async () => {
+    getLiveFeeds($(selector).val())
+  }, 15000);
+}
+
+async function getLiveFeeds(stationCode) {
+  $(".loader").toggle("show");
+  let resposne = await fetch(`/${stationCode}/train/status`, {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    }
+  });
+  let result = await resposne.json();
+  $(".loader").toggle("hide");
+}
